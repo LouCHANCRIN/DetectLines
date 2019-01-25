cac469085e31b9c44e198214b0c4c862570afff7
lchancri <lchancri@student.42.fr>
2019-01-25 18:11:44 +0100

    Now work with video, added average_slope to have a smoother line

--- main.py
+++ main.py
@@ -2,6 +2,40 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def make_coordinates(image, line_params):
+    slope, intercept = line_params
+    y1 = np.shape(image)[0]
+    y2 = int(y1 * (2/5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    ret = np.array([x1, y1, x2, y2])
+    return (ret)
+
+def average_slope(image, lines):
+    left_fit = []
+    right_fit = []
+    count_left = 0
+    count_right = 0
+    for x1, y1, x2, y2 in lines:
+        #np.polyfit give the slope and the y intercept of the line
+        params = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = params[0]
+        intercept = params[1]
+        if (slope < 0):
+            left_fit.append((slope, intercept))
+            count_left += 1
+        else:
+            right_fit.append((slope, intercept))
+            count_right += 1
+
+    if (count_right == 0 or count_left == 0):
+        return (lines)
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+    return (np.array([left_line, right_line]))
+
 def canny(color_image):
     image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
 
@@ -26,13 +60,11 @@ def region_of_interest(image):
 def create_line_image(image, lines):
     line_image = np.zeros_like(image)
     if (lines is not None):
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 5)
     return (line_image)
 
-
-def main():
+def main_image():
     color_image = cv2.imread("test_image.jpg")
     image = canny(color_image)
 
@@ -41,13 +73,44 @@ def main():
     #plt.show()
     roi = region_of_interest(image)
 
+    #Hough transform find the line that best describe our points
+    #2 mean 2pixel precision
+    #using np.pi to have a precision of 1 degree in radian
+    #100 is the threshold of vote per bin
+    #minLineLength=X means that we dont accept line smaller than X pixels
+    #maxLineGap=X means that if 2 line have a gap <=X pixels we can connect them in a single line
     lines = cv2.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]),
-            minLineLength=40, maxLineGap=5)#using np.pi to have radians
-    line2 = create_line_image(color_image, lines)
-    image_with_line = cv2.addWeighted(color_image, 0.8, line2, 1, 1)
+            minLineLength=40, maxLineGap=200)
 
-    cv2.imshow("image", image_with_line)
+    lines = np.reshape(lines, (np.shape(lines)[0], 4))
+    averaged_line = average_slope(color_image, lines)
+    line_image = create_line_image(color_image, averaged_line)
+    image_with_line = cv2.addWeighted(color_image, 0.8, line_image, 1, 1)
+    cv2.imshow("Image", image_with_line)
     cv2.waitKey(0)
 
+def main_video():
+    vid = cv2.VideoCapture("test2.mp4")
+    print("press q to quit")
+    while (vid.isOpened()):
+        ret, frame = vid.read()
+        if (ret == False):
+            break
+        image = canny(frame)
+        roi = region_of_interest(image)
+        lines = cv2.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]),
+                minLineLength=40, maxLineGap=10)
+        lines = np.reshape(lines, (np.shape(lines)[0], 4))
+        averaged_line = average_slope(frame, lines)
+        line_image = create_line_image(frame, averaged_line)
+        image_with_line = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+        cv2.imshow("Video", image_with_line)
+        if (cv2.waitKey(1) & 0xFF == ord('q')):
+            break
+
+    vid.release()
+    cv2.destroyAllWindows()
+
 if __name__ == '__main__':
-    main()
+    main_video()
+    #main_image()
